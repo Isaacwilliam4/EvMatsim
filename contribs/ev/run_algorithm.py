98db9dd1b01ad5e359159c8f1ff28483ba3d875a
Isaac Peterson <isaacwilliam4@gmail.com>
2024-11-08 17:15:17 -0700

    maybe fixed bug that was activated when there were still cars in the queue

--- contribs/ev/run_algorithm.py
+++ contribs/ev/run_algorithm.py
@@ -9,13 +9,45 @@ from evsim.util import *
 from evsim.scripts.create_population import *
 from pathlib import Path
 
+def update_Q(Q: pd.DataFrame, chosen_links, score):
+    # Set 'link_id' as the index for faster lookups
+    Q = Q.set_index("link_id")
+
+    for link in chosen_links:
+        link = int(link)
+        if link in Q.index:
+            row = Q.loc[link]
+            avg_score = row["average_reward"]
+            count = row["count"]
+
+            new_score = ((avg_score * count) + score) / (count + 1)
+            count += 1
+
+            # Update the row with new values
+            Q.loc[link, "average_reward"] = new_score
+            Q.loc[link, "count"] = count
+        else:
+            print(f"\n\n\n\ Link {link} not found in Q DataFrame \n\n\n")
+
+    # Reset the index if you need to work with the 'link_id' as a column again
+    Q = Q.reset_index()
+
+    return Q
+
+def save_Q(Q: pd.DataFrame, q_path):
+    Q.to_csv(q_path, index=False)
+
+
+def load_Q(q_path):
+    return pd.read_csv(q_path)
+
+
 def main(args):
     if args.num_agents:
         node_coords = get_node_coords(args.network_path)
         create_population_and_plans_xml(args.num_agents, node_coords, args.plans_path)
     num_runs = args.num_runs
-    NUM_MATSIM_ITERS = args.num_matsim_iters
-    update_last_iteration(args.config_path, NUM_MATSIM_ITERS - 1)
+    update_last_iteration(args.config_path, args.num_matsim_iters - 1)
     algorithm_results = pd.DataFrame(columns=["iteration", "avg_score", "selected_links"])
     link_ids = get_link_ids(args.network_path)
     
@@ -37,16 +69,57 @@ def main(args):
     if args.min_ram and args.max_ram:
         os.environ["MAVEN_OPTS"] = f"-Xms{args.min_ram}g -Xmx{args.max_ram}g"
 
+    if os.path.exists(args.q_path):
+        Q = load_Q(args.q_path)
+    else:
+        Q = pd.DataFrame(
+            {
+                "link_id": link_ids,
+                "average_reward": np.zeros_like(link_ids, dtype=float),
+                "count": np.zeros_like(link_ids, dtype=int),
+            }
+        )
+
+    # Explore phase: random link selection without replacement
+    explored_links = np.random.permutation(link_ids)
+    for i in range(args.explore_steps):
+        # Display run status
+        print(f"\033[36m\n{'#' * 62}\033[0m")
+        print(f"\033[1m\033[32m# {'EXPLORATION':^58} #\033[0m")
+        print(f"\033[1m\033[32m# {f'{i+1}/{args.explore_steps}':^58} #\033[0m")
+        print(f"\033[36m{'#' * 62}\033[0m\n")
+        if len(explored_links) < args.num_chargers:
+            explored_links = np.random.permutation(link_ids)
+        chosen_links, explored_links = (
+            explored_links[:args.num_chargers],
+            explored_links[args.num_chargers:],
+        )
+
+        create_chargers_xml(chosen_links, args.chargers_path)
+        # Run matsim in Java
+        os.system(f'mvn exec:java -Dexec.args="{args.config_path}"')
+        scores = pd.read_csv(os.path.join(args.output_path, "scorestats.csv"), sep=";")
+        average_score = scores["avg_executed"].iloc[-1]
+
+        # Update Q values
+        Q = update_Q(Q, chosen_links, average_score)
+        save_Q(Q, args.q_path)
+
     for i in range(1, num_runs + 1):
         print_run_info(i, num_runs)
 
         chosen_links = monte_carlo_algorithm(args.num_chargers, link_ids, algorithm_results)
         create_chargers_xml(chosen_links, args.chargers_path)
 
-        subprocess.run([f"mvn exec:java -Dexec.args={os.path.abspath(args.config_path)}"], shell=True)
+        os.system(f'mvn -e exec:java -Dexec.args="{args.config_path}" > ./matsimrun.log')
         scores = pd.read_csv(os.path.join(args.output_path, "scorestats.csv"), sep=";")
 
         average_score = scores["avg_executed"].iloc[-1]
+
+        # Update Q values
+        Q = update_Q(Q, chosen_links, average_score)
+        save_Q(Q, args.q_path)
+
         row = pd.DataFrame({"iteration": [i], "avg_score": [average_score], "selected_links": [chosen_links.tolist()]})
         algorithm_results = pd.concat([algorithm_results, row], ignore_index=True)
 
@@ -77,7 +150,7 @@ def print_run_info(current_run, total_runs):
     print(f"{CYAN}#" * 62 + RESET + "\n")
 
 if __name__ == "__main__":
-    argparser = argparse.ArgumentParser()
+    argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     argparser.add_argument("config_path", type=str, help="Path to the matsim config.xml file")
     argparser.add_argument("network_path", type=str, help="Path to the matsim network.xml file")
@@ -85,7 +158,11 @@ if __name__ == "__main__":
     argparser.add_argument("chargers_path", type=str, help="Path to the output matsim chargers.xml file")
     argparser.add_argument("results_path", type=str, help="Directory where results will be saved")
     argparser.add_argument("output_path", type=str, help="Path to the output directory created by matsim")
-
+    argparser.add_argument("q_path",help="Path to q file containing dataframe hold link average reward, \
+                           will be created if doesn't exist, should be .csv file", type=str)
+    argparser.add_argument("--algorithm",help="Algorithm to use for optimization",choices=["montecarlo"],
+                            default="montecarlo",type=str)
+    argparser.add_argument("--explore_steps", help="Number of steps to explore", default=0, type=int)
     argparser.add_argument("--alg_prefix", required=True, default="my_algorithm", type=str, help="Prefix for your algorithm")
     argparser.add_argument("--num_runs", default=50, type=int, help="Number of iterations for the algorithm")
     argparser.add_argument("--num_matsim_iters", default=5, type=int, help="Number of iterations for the matsim simulator")
