5f4991e7977155019c6daf25b60767c9967481d0
Isaac Peterson <isaacwilliam4@gmail.com>
2025-01-16 16:24:34 -0700

    progress on creating graph from xml network file

--- contribs/ev/evsim/scripts/create_graph_from_xml_network.py
+++ contribs/ev/evsim/scripts/create_graph_from_xml_network.py
@@ -6,7 +6,7 @@ import torch
 #TODO create class for matsim link to handle the link attrbutes
 
 class MatsimXMLData(Dataset):
-    def __init__(self, network_xml_path, charger_xml_path, transform=None):
+    def __init__(self, network_xml_path, charger_xml_path, charger_dict, transform=None):
         super().__init__(transform=transform)
         self.network_xml_path = network_xml_path
         self.charger_xml_path = charger_xml_path
@@ -17,6 +17,8 @@ class MatsimXMLData(Dataset):
         self.edge_mapping = {}
         self.edge_attr_mapping = {}
         self.graph = Data()
+        self.charger_dict = charger_dict
+        self.num_charger_types = len(charger_dict)
         self.parse_matsim_network()
 
     def len(self):
@@ -26,14 +28,16 @@ class MatsimXMLData(Dataset):
         return self.data_list[idx]
     
     def parse_matsim_network(self):
+        """Parse the matsim network provided via the self.network_xml_path and creates a
+        torch_geometric.data.Data object with the network information. Then assigns the 
+        Data object to self.graph.
+        """
         tree = ET.parse(self.network_xml_path)
         root = tree.getroot()
-        link_ids = []
         matsim_node_ids = []
         node_ids = []
         node_pos = []
         edge_index = []
-        edge_attr = []
 
         for i, node in enumerate(root.findall(".//node")):
             node_id = node.get("id")
@@ -43,31 +47,46 @@ class MatsimXMLData(Dataset):
             node_ids.append(i)
 
         create_edge_attr_mapping = True
+
+        first_link = root.find(".//link")
+        link_attr = len(first_link.items())
+
+        for i, key in enumerate(first_link.items()):
+            self.edge_attr_mapping[key] = i
+
+        for i, key in enumerate(self.charger_dict.keys()):
+            self.edge_attr_mapping[key] = i + len(first_link.items())
     
-        for link in root.findall(".//link"):
+        for i, link in enumerate(root.findall(".//link")):
             link_id = link.get("id")
-            link_ids.append(link_id)
             from_node = link.get("from")
             to_node = link.get("to")
             from_idx = self.node_mapping[from_node]
             to_idx = self.node_mapping[to_node]
             edge_index.append([from_idx, to_idx])
             curr_link_attr = []
-            attrib_id = 0
 
             for key, value in link.items():
                 if value.isnumeric():
                     curr_link_attr.append(torch.tensor(float(value), dtype=torch.float))
-                    if create_edge_attr_mapping:
-                        self.edge_attr_mapping[attrib_id] = key
-                        attrib_id += 1
-            create_edge_attr_mapping = False
-            edge_attr.append(curr_link_attr)
+
 
         self.graph.x = torch.tensor(node_ids).view(-1, 1)
         self.graph.pos = torch.tensor(node_pos)
         self.graph.edge_index = torch.tensor(edge_index).t()
         self.graph.edge_attr = torch.tensor(edge_attr)
+
+    def parse_charger_network(self):
+        tree = ET.parse(self.network_xml_path)
+        root = tree.getroot()
+
+        for charger in root.findall(".//charger"):
+            link_id = charger.get("link")
+            charger_type = charger.get("type")
+
+             
+            self.graph.edge_attr[self.edge_mapping[link_id]]
+        
 
     def get_graph(self):
         return self.graph
@@ -77,5 +96,13 @@ class MatsimXMLData(Dataset):
 if __name__ == "__main__":
     network_xml_path = "/home/isaacp/repos/EvMatsim/contribs/ev/scenarios/utahev/utahevnetwork.xml"
     charger_xml_path = "/home/isaacp/repos/EvMatsim/contribs/ev/scenarios/utahev/utahevchargers.xml"
-    dataset = MatsimXMLData(network_xml_path, charger_xml_path)
+    charger_dict = {
+        "none": 0,
+        # in matsim the default charger is a static charger we could update this dictionary
+        # to include different charger types along with charger cost and other attributes
+        # the graph uses this dictionary to map the charger type to an integer
+        "default": 1,
+        "dynamic": 2
+    }
+    dataset = MatsimXMLData(network_xml_path, charger_xml_path, charger_dict)
     print(dataset.node_mapping)
