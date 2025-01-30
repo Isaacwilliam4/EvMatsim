e1b9e51afcfc7171d0a50d6b8d9c66afabc797cb
Chandler J <cjustice2000@gmail.com>
2025-01-30 12:33:09 -0700

    started trying to aggregate scraping

--- contribs/ev/evsim/scripts/scrape_pems.py
+++ contribs/ev/evsim/scripts/scrape_pems.py
@@ -1,10 +1,12 @@
 from urllib.request import urlopen, Request
 from urllib.parse import urlencode
 from datetime import datetime
+import pandas as pd
+import io
 
 def get_pems_timeseries_report(
         phpsessid: str,
-        station_id: str,
+        sensor_id: str,
         start_time: datetime,
         end_time: datetime,
         query_for: str = "flow",
@@ -15,7 +17,8 @@ def get_pems_timeseries_report(
     pull data about the flow of traffic at a given station. 
 
     @Input
-    station_id: station_id where you want to capture data
+    phpsessid: session ID for logging into PeMS. Can be found by inspecting header of request to PeMS.
+    sensor_id: sensor_id where you want to capture data
     start_time: point in time to begin fetching data
     stop_time: point in time to stop fetching data
     query_for: type of query desired (ie, flow, occupancy, speed, etc)
@@ -27,8 +30,9 @@ def get_pems_timeseries_report(
 
     start_time_unix, end_time_unix = int(start_time.timestamp()), int(end_time.timestamp())
 
-    ref_loc = f"https://udot.iteris-pems.com/?report_form=1&dnode=VDS&content=loops&export=&station_id={station_id}&s_time_id={start_time_unix}&e_time_id={end_time_unix}&tod=all&tod_from=0&tod_to=0&dow_0=on&dow_1=on&dow_2=on&dow_3=on&dow_4=on&dow_5=on&dow_6=on&holidays=on&q={query_for}&q2={query_for_two if query_for_two else ""}&gn=hour&agg=on&html.x=66&html.y=5"
+    ref_loc = f"https://udot.iteris-pems.com/?report_form=1&dnode=VDS&content=loops&export=&station_id={sensor_id}&s_time_id={start_time_unix}&e_time_id={end_time_unix}&tod=all&tod_from=0&tod_to=0&dow_0=on&dow_1=on&dow_2=on&dow_3=on&dow_4=on&dow_5=on&dow_6=on&holidays=on&q={query_for}&q2={query_for_two if query_for_two else ""}&gn=hour&agg=on&html.x=66&html.y=5"
     cookie = f"PHPSESSID={phpsessid}"
+
 
     # Prepare query parameters
     params = {
@@ -36,7 +40,7 @@ def get_pems_timeseries_report(
         "dnode": "VDS",
         "content": "loops",
         "export": "text",
-        "station_id": station_id,
+        "station_id": sensor_id,
         "s_time_id": start_time_unix,
         "e_time_id": end_time_unix,
         "tod": "all",
@@ -79,12 +83,48 @@ def get_pems_timeseries_report(
     base_url = "https://udot.iteris-pems.com/"
     url_query = urlencode(params)
     full_url = f"{base_url}?{url_query}"
-
     req = Request(full_url, headers=headers)
     with urlopen(req) as res:
         decoded = res.read().decode('utf-8')
-        print(decoded)
+        # print(decoded)
     return decoded
 
+
+def aggregate_flow_sensor_data(
+        phpsessid: str,
+        sensor_id: str,
+        start_time: datetime,
+        end_time: datetime):
+    """
+    This will use the get_pems_timeseries_report to grab sensor data 
+    and then produce an average at each hour. After grabbing the data
+    computes the average flow at each hour and spits out a dataframe
+    which contains the sensor and its associated flow values at each
+    hour.
+    """
+    data = get_pems_timeseries_report(phpsessid,
+                               sensor_id, 
+                               start_time, 
+                               end_time
+                               )
+    # Read the data using read_csv with space-based separator
+    df = pd.read_csv(io.StringIO(data), sep=r'\s+', engine='python')
+    
+    # Rename columns for clarity
+    df.columns = ["Date", "Time", "Flow (Veh/Hour)", "Lane Points", "% Observed"]
+    
+    # Combine "Date" and "Time" into a single datetime column
+    df["Hour"] = pd.to_datetime(df["Date"] + " " + df["Time"], format="%m/%d/%Y %H:%M")
+    
+    # Drop the old "Date" and "Time" columns
+    df = df[["Hour", "Flow (Veh/Hour)", "Lane Points", "% Observed"]]
+    print(df)
+
+    return df
+
 if __name__ == "__main__":
-    get_pems_timeseries_report("3fccfd59b72f5784ae8262c7492b6d51","993103220", datetime(2024, 10, 1), datetime(2024, 10, 2))
+    aggregate_flow_sensor_data("3fccfd59b72f5784ae8262c7492b6d51",
+                               "993103220", 
+                               datetime(2024, 10, 1), 
+                               datetime(2024, 10, 2)
+                               )
