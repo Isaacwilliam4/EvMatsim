bc5a06c729b9a294bcb268eabe6aeaa6fa56cbd0
Isaac Peterson <isaac.peterson@usu.edu>
2025-02-04 13:07:47 -0700

    got the training loop working with gymnasium, had to decrease the learning rate to 0.00001 in order to avoid NANs in the adam optimizer

--- contribs/ev/rl_algorithm.py
+++ contribs/ev/rl_algorithm.py
@@ -15,14 +15,21 @@ def main(args: argparse.Namespace):
 
     env = SubprocVecEnv([make_env for _ in range(args.num_envs)])
 
-    model = PPO("MlpPolicy", env, verbose=1, device='cpu', tensorboard_log=f"{args.results_dir}/{datetime.now().strftime('%Y%m%d_%H%M%S_%f')}/")
+    model = PPO("MlpPolicy", 
+                env, 
+                n_steps=1, 
+                verbose=1, 
+                device='cpu', 
+                tensorboard_log=f"{args.results_dir}/{datetime.now().strftime('%Y%m%d_%H%M%S_%f')}/",
+                batch_size=2,
+                learning_rate=0.00001)
 
     model.learn(total_timesteps=10)
     model.save("ppo_matsim")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train a PPO model on the MatsimGraphEnv.')
-    parser.add_argument('--num_envs', type=int, default=10, help='Number of environments to run in parallel.')
+    parser.add_argument('--num_envs', type=int, default=2, help='Number of environments to run in parallel.')
     parser.add_argument('--results_dir', type=str, default=os.path.abspath('./ppo_results'), help='where to save tensorboard logs and model checkpoints.')
 
     args = parser.parse_args()
