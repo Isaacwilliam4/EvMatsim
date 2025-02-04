0e5bf58b14ac7c5d6254b8af6d4c61d63c79c8ee
Isaac Peterson <isaac.peterson@usu.edu>
2025-02-04 14:35:59 -0700

    progress on proper logging of reward in tensorflow

--- contribs/ev/rl_algorithm.py
+++ contribs/ev/rl_algorithm.py
@@ -2,16 +2,35 @@ import gymnasium as gym
 import evsim.envs
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.callbacks import BaseCallback
 import argparse
 import os
 from datetime import datetime
+from pathlib import Path
+
+class TensorboardCallback(BaseCallback):
+    """
+    Custom callback for plotting additional values in tensorboard.
+    """
+
+    def __init__(self, verbose=0):
+        super(TensorboardCallback, self).__init__(verbose)
+
+    def _on_step(self) -> bool:
+        avg_reward = 0
+        for i, infos in enumerate(self.training_env.reset_infos):
+            avg_reward += infos['reward']
+
+        self.logger.record('Avg Reward', (avg_reward/(i+1)))
+        return True
+
 
 def main(args: argparse.Namespace):
     if not os.path.exists(args.results_dir):
         os.makedirs(args.results_dir)
 
     def make_env():
-        return gym.make("MatsimGraphEnv-v0")
+        return gym.make("MatsimGraphEnv-v0", config_path = args.matsim_config)
 
     env = SubprocVecEnv([make_env for _ in range(args.num_envs)])
 
@@ -24,13 +43,15 @@ def main(args: argparse.Namespace):
                 batch_size=2,
                 learning_rate=0.00001)
 
-    model.learn(total_timesteps=10)
+    model.learn(total_timesteps=10, callback=TensorboardCallback())
     model.save("ppo_matsim")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Train a PPO model on the MatsimGraphEnv.')
     parser.add_argument('--num_envs', type=int, default=2, help='Number of environments to run in parallel.')
-    parser.add_argument('--results_dir', type=str, default=os.path.abspath('./ppo_results'), help='where to save tensorboard logs and model checkpoints.')
+    parser.add_argument('--results_dir', type=str, default=Path(Path(__file__).parent, 'ppo_results'), \
+                        help='where to save tensorboard logs and model checkpoints.')
+    parser.add_argument('--matsim_config', type=str, help='path to the matsim config.xml file')
 
     args = parser.parse_args()
 
