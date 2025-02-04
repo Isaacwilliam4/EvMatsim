e779ff171cd06efb916243905cb7e755bbdb7cf4
Isaac Peterson <isaac.peterson@usu.edu>
2025-02-04 16:55:24 -0700

    started saving best configurations for chargers

--- contribs/ev/rl_algorithm.py
+++ contribs/ev/rl_algorithm.py
@@ -8,31 +8,34 @@ import os
 from datetime import datetime
 from pathlib import Path
 from evsim.envs.matsim_graph_env import MatsimGraphEnv
-SAVE_DIR = ''
+import numpy as np
 
 class TensorboardCallback(BaseCallback):
     """
     Custom callback for plotting additional values in tensorboard.
     """
 
-    def __init__(self, verbose=0):
+    def __init__(self, verbose=0, save_dir=None):
         super(TensorboardCallback, self).__init__(verbose)
+        self.save_dir = save_dir
+        self.max_reward = -np.inf
+        self.best_env: MatsimGraphEnv = None
 
     def _on_step(self) -> bool:
-        best_env: MatsimGraphEnv = None
-        max_reward = 0
         avg_reward = 0
 
         for i, infos in enumerate(self.locals['infos']):
-            env_inst: MatsimGraphEnv = infos['graph_env_list']
+            env_inst: MatsimGraphEnv = infos['graph_env_inst']
             reward = env_inst.reward
             avg_reward += reward
-            if reward > max_reward:
-                max_reward = reward
-                best_env = env_inst
+            if reward > self.max_reward:
+                self.max_reward = reward
+                self.best_env = env_inst
+                self.best_env.save_charger_config_to_csv(Path(self.save_dir, 'best_chargers.csv'))
 
-        best_env.dataset.save_charger_config_to_csv(Path(SAVE_DIR, 'chargers.csv'))
         self.logger.record('Avg Reward', (avg_reward/(i+1)))
+        self.logger.record('Best Reward', self.max_reward)
+
         return True
 
 
@@ -40,26 +43,27 @@ def main(args: argparse.Namespace):
     if not os.path.exists(args.results_dir):
         os.makedirs(args.results_dir)
 
-    SAVE_DIR = f"{args.results_dir}/{datetime.now().strftime('%Y%m%d_%H%M%S_%f')}/"
+    save_dir = f"{args.results_dir}/{datetime.now().strftime('%Y%m%d_%H%M%S_%f')}/"
 
     def make_env():
-        return gym.make("MatsimGraphEnv-v0", config_path = args.matsim_config)
+        return gym.make("MatsimGraphEnv-v0", config_path=args.matsim_config, num_agents=1)
 
     env = SubprocVecEnv([make_env for _ in range(args.num_envs)])
     # n_steps: refers to the number of steps for each environment to collect data before
     # a batch is processed
     # batch_size: the amount of data that is sampled every n_steps from the replay buffer
+    # total samples = num_envs * iterations
     model = PPO("MlpPolicy", 
                 env, 
                 n_steps=1, 
                 verbose=1, 
                 device='cpu', 
-                tensorboard_log=SAVE_DIR,
+                tensorboard_log=save_dir,
                 batch_size=2,
                 learning_rate=0.00001)
     
     # total_timesteps = n_steps * num_envs * iterations
-    model.learn(total_timesteps=10, callback=TensorboardCallback())
+    model.learn(total_timesteps=10, callback=TensorboardCallback(save_dir=save_dir))
     model.save(Path(args.results_dir, "ppo_matsim"))
 
 if __name__ == "__main__":
