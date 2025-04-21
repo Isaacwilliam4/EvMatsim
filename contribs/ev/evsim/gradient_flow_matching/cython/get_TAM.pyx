cac5d377c7cafcc5b2c8b651feee73b234b18f8f
Isaac Peterson <isaac.peterson@usu.edu>
2025-04-21 14:14:02 -0600

    updated cython to precompute adjacency matrix instead of computing it every bfs call

--- contribs/ev/evsim/gradient_flow_matching/cython/get_TAM.pyx
+++ contribs/ev/evsim/gradient_flow_matching/cython/get_TAM.pyx
@@ -3,7 +3,7 @@ from libcpp.queue cimport queue
 import numpy as np
 cimport numpy as np
 cimport cython
-from tqdm import tqdm  
+from tqdm import tqdm
 import psutil
 
 cdef dict path_cache = {}
@@ -15,27 +15,36 @@ def is_enough_memory(double threshold_gb=10.0):
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cdef vector[int] bfs(int source, int target, int n_nodes,
-                     np.ndarray[np.int32_t, ndim=2] edge_index,
-                     bint use_memoization,
-                     double gb_threshold):
+cdef vector[vector[int]] build_adjacency_list(np.ndarray[np.int32_t, ndim=2] edge_index, int n_nodes):
     cdef:
         int i, u, v
+        int n_edges = edge_index.shape[0]
         vector[vector[int]] adj = vector[vector[int]](n_nodes)
+    for i in range(n_edges):
+        u = edge_index[i, 0]
+        v = edge_index[i, 1]
+        adj[u].push_back(v)
+    return adj
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef vector[int] bfs(int source, int target, int n_nodes,
+                     np.ndarray[np.int32_t, ndim=2] edge_index,
+                     vector[vector[int]]& adj,
+                     bint use_memoization,
+                     double gb_threshold):
+
+    cdef:
+        int i, u, v
         vector[int] visited = vector[int](n_nodes, 0)
         vector[int] prev = vector[int](n_nodes, -1)
         queue[int] q
         vector[int] edge_path, reversed_path
+        int n_edges = edge_index.shape[0]
         tuple cache_key = (source, target)
 
-    if use_memoization:
-        if cache_key in path_cache:
-            return path_cache[cache_key]
-
-    for i in range(edge_index.shape[0]):
-        u = edge_index[i, 0]
-        v = edge_index[i, 1]
-        adj[u].push_back(v)
+    if use_memoization and cache_key in path_cache:
+        return path_cache[cache_key]
 
     visited[source] = 1
     q.push(source)
@@ -55,7 +64,7 @@ cdef vector[int] bfs(int source, int target, int n_nodes,
     v = target
     while prev[v] != -1:
         u = prev[v]
-        for i in range(edge_index.shape[0]):
+        for i in range(n_edges):
             if edge_index[i, 0] == u and edge_index[i, 1] == v:
                 edge_path.push_back(i)
                 break
@@ -64,11 +73,8 @@ cdef vector[int] bfs(int source, int target, int n_nodes,
     for i in range(edge_path.size() - 1, -1, -1):
         reversed_path.push_back(edge_path[i])
 
-    if use_memoization:
-        # don't add to cache if less than gb_threshold of memory left
-        use_cache = is_enough_memory(gb_threshold)
-        if use_cache:
-            path_cache[cache_key] = reversed_path
+    if use_memoization and is_enough_memory(gb_threshold):
+        path_cache[cache_key] = reversed_path
 
     return reversed_path
 
@@ -89,8 +95,9 @@ def get_TAM(dict cluster_lists,
         object origins, dests
         vector[int] path
         np.ndarray[np.float64_t, ndim=3] TAM = np.zeros((n_edges, n_clusters, n_clusters), dtype=np.float64)
-    
-    pbar = tqdm(total=(n_clusters*(n_clusters-1)), desc="Creating TAM")
+        vector[vector[int]] adj = build_adjacency_list(edge_index, n_nodes)
+
+    pbar = tqdm(total=(n_clusters * (n_clusters - 1)), desc="Creating TAM")
     for cluster1 in range(n_clusters):
         for cluster2 in range(n_clusters):
             if cluster1 == cluster2:
@@ -103,10 +110,10 @@ def get_TAM(dict cluster_lists,
             for i in range(n_samples):
                 origin = origins[np.random.randint(0, len(origins))]
                 dest = dests[np.random.randint(0, len(dests))]
-                path = bfs(origin, dest, n_nodes, edge_index, use_memoization, gb_threshold)
+                path = bfs(origin, dest, n_nodes, edge_index, adj, use_memoization, gb_threshold)
                 for idx in path:
                     TAM[idx, cluster1, cluster2] += 1
 
     TAM = TAM / n_samples
-
     return TAM
+
