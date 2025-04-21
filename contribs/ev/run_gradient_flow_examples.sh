3dd388dc55373ab6a45c989fa72425d07589b0ba
Isaac Peterson <isaac.peterson@usu.edu>
2025-04-21 20:29:01 +0000

    added examples script for running all experiments

--- contribs/ev/run_gradient_flow_examples.sh
+++ contribs/ev/run_gradient_flow_examples.sh
@@ -1,5 +1,6 @@
 eval "$(conda shell.bash hook)"
 conda activate matsimenv
+
 python3 -m evsim.gradient_flow_matching.run_gradient_flow_matching \
 "./evsim/gradient_flow_matching/utah_flow_results" \
 "./scenario_examples/utah_flow_scenario_example/utahnetwork.xml" \
@@ -10,5 +11,34 @@ python3 -m evsim.gradient_flow_matching.run_gradient_flow_matching \
 "--log_interval" "1_000" \
 "--save_interval" "-1"
 
+python3 -m evsim.gradient_flow_matching.run_gradient_flow_matching \
+"./evsim/gradient_flow_matching/utah_flow_results" \
+"./scenario_examples/utah_flow_scenario_example/utahnetwork.xml" \
+"./scenario_examples/utah_flow_scenario_example/utahcounts.xml" \
+"--num_clusters" "100" \
+"--num_samples" "1_000" \
+"--training_steps" "1_000_000" \
+"--log_interval" "1_000" \
+"--save_interval" "-1"
+
+python3 -m evsim.gradient_flow_matching.run_gradient_flow_matching \
+"./evsim/gradient_flow_matching/utah_flow_results" \
+"./scenario_examples/utah_flow_scenario_example/utahnetwork.xml" \
+"./scenario_examples/utah_flow_scenario_example/utahcounts.xml" \
+"--num_clusters" "200" \
+"--num_samples" "100" \
+"--training_steps" "1_000_000" \
+"--log_interval" "1_000" \
+"--save_interval" "-1"
+
+python3 -m evsim.gradient_flow_matching.run_gradient_flow_matching \
+"./evsim/gradient_flow_matching/utah_flow_results" \
+"./scenario_examples/utah_flow_scenario_example/utahnetwork.xml" \
+"./scenario_examples/utah_flow_scenario_example/utahcounts.xml" \
+"--num_clusters" "200" \
+"--num_samples" "1_000" \
+"--training_steps" "1_000_000" \
+"--log_interval" "1_000" \
+"--save_interval" "-1"
 
 
