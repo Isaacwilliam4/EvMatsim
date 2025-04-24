05e50dba44bd46c2305befc0a2d235ca2e70f0e9
Isaac Peterson <isaac.peterson@usu.edu>
2025-04-24 13:58:48 -0600

    asdf

--- run_matsim_sims.sh
+++ run_matsim_sims.sh
@@ -1,5 +1,5 @@
 cd matsim/
-export MAVEN_OPTS="-Xmx100G"
-mvn exec:java -Dexec.mainClass="org.matsim.run.RunMatsim" -Dexec.args="../contribs/ev/scenario_examples/cluster_scenarios/utah_flow_scenario_example_1_000_000/utahconfig.xml" -Dexec.jvmArgs="-Xmx100G"
-mvn exec:java -Dexec.mainClass="org.matsim.run.RunMatsim" -Dexec.args="../contribs/ev/scenario_examples/cluster_scenarios/utah_flow_scenario_example_5_000_000/utahconfig.xml" -Dexec.jvmArgs="-Xmx100G"
-mvn exec:java -Dexec.mainClass="org.matsim.run.RunMatsim" -Dexec.args="../contribs/ev/scenario_examples/cluster_scenarios/utah_flow_scenario_example_10_000_000/utahconfig.xml" -Dexec.jvmArgs="-Xmx100G"
+export MAVEN_OPTS="-Xmx60G"
+mvn exec:java -Dexec.mainClass="org.matsim.run.RunMatsim" -Dexec.args="../contribs/ev/scenario_examples/cluster_scenarios/utah_flow_scenario_example_1_000_000/utahconfig.xml"
+mvn exec:java -Dexec.mainClass="org.matsim.run.RunMatsim" -Dexec.args="../contribs/ev/scenario_examples/cluster_scenarios/utah_flow_scenario_example_2_000_000/utahconfig.xml"
+mvn exec:java -Dexec.mainClass="org.matsim.run.RunMatsim" -Dexec.args="../contribs/ev/scenario_examples/cluster_scenarios/utah_flow_scenario_example_3_000_000/utahconfig.xml"
