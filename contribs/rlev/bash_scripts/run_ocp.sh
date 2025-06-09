dc0a4c0f654b9fa87bdbef69278267270e49b69e
Isaac Peterson <isaacwilliam4@gmail.com>
2025-06-09 09:59:26 -0600

    progress on running server

--- contribs/rlev/bash_scripts/run_ocp.sh
+++ contribs/rlev/bash_scripts/run_ocp.sh
@@ -7,12 +7,12 @@ conda activate ppomatsimenv
 python -m rlev.rl_algorithm_ppo \
 "/home/isaacp/repos/EvMatsim/contribs/rlev/scenario_examples/i-15-scenario/i-15-config.xml" \
 "--num_timesteps" "1000000" \
-"--num_envs" "6" \
+"--num_envs" "16" \
 "--num_steps" "1" \
-"--batch_size" "6" \
-"--results_dir" "/home/isaacp/repos/EvMatsim/contribs/rlev/i-15-single-agent-results" \
+"--batch_size" "16" \
+"--results_dir" "/home/isaacp/repos/EvMatsim/contribs/rlev/i-15-results" \
 "--learning_rate" "0.0001" \
 "--clip_range" "0.3" \
 "--save_frequency" "20000" \
-"--policy_type" "MlpPolicy"
+"--policy_type" "MlpPolicy" > client_ouput.log 2>&1
 
--- contribs/rlev/bash_scripts/run_ocp_server.sh
+++ contribs/rlev/bash_scripts/run_ocp_server.sh
@@ -1,3 +1,3 @@
 cd ../
-export MAVEN_OPTS="-Xmx61G"
-mvn exec:java -Dexec.mainClass="org.matsim.contrib.rlev.OCPRewardServer" -Dexec.args="16"
+export MAVEN_OPTS="-Xmx240G"
+mvn exec:java -Dexec.mainClass="org.matsim.contrib.rlev.OCPRewardServer" -Dexec.args="20" > server_output.log 2>&1
