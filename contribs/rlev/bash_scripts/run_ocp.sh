27e2107a9f79ed2a084ad3d9240f1f76bba802ff
Isaac Peterson <isaac.peterson@usu.edu>
2025-06-09 09:53:57 -0600

    fixed issue with server running matsim

--- contribs/rlev/bash_scripts/run_ocp.sh
+++ contribs/rlev/bash_scripts/run_ocp.sh
@@ -7,10 +7,10 @@ conda activate ppomatsimenv
 python -m rlev.rl_algorithm_ppo \
 "/home/isaacp/repos/EvMatsim/contribs/rlev/scenario_examples/i-15-scenario/i-15-config.xml" \
 "--num_timesteps" "1000000" \
-"--num_envs" "6" \
+"--num_envs" "2" \
 "--num_steps" "1" \
-"--batch_size" "6" \
-"--results_dir" "/home/isaacp/repos/EvMatsim/contribs/rlev/i-15-single-agent-results" \
+"--batch_size" "2" \
+"--results_dir" "/home/isaacp/repos/EvMatsim/contribs/rlev/i-15-results" \
 "--learning_rate" "0.0001" \
 "--clip_range" "0.3" \
 "--save_frequency" "20000" \
